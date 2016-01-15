e20a2566720c24d77c1186bcaf7a964a565a2c7d
Ben Zalatan <bzalatan1@gmail.com>
2016-01-14 22:50:56 -0500

    Added simple test FTP class.

---
+++ src/Testing/FTPTest.java
@@ -0,0 +1,44 @@
+package Testing;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class FTPTest {
+	//http://www.codejava.net/java-se/networking/ftp/upload-files-to-ftp-server-using-urlconnection-class
+	private String host = "127.0.0.1:80",
+			userName = "12345",
+			password = "12345",
+			filePath = "program.jar",
+			remoteFilePath = "/programs/program.jar";
+	private final int BUFFER_SIZE = 4096;
+	
+	public FTPTest(String ip, String un, String pw, String path, String remotePath) {
+		host = ip;
+		un = userName;
+		pw = password;
+		filePath = path;
+		remoteFilePath = remotePath;
+	}
+	
+	public void save() throws IOException {
+	    URL url = new URL(formatURL());
+	    URLConnection connection = url.openConnection();
+	    OutputStream outputStream = connection.getOutputStream();
+	    FileInputStream inputStream = new FileInputStream(filePath);
+
+	    byte[] buffer = new byte[BUFFER_SIZE];
+	    int bytesRead = -1;
+	    while ((bytesRead = inputStream.read(buffer)) != -1)
+	        outputStream.write(buffer, 0, bytesRead);
+
+	    inputStream.close();
+	    outputStream.close();
+	}
+	
+	private String formatURL() {
+		return "ftp://" + userName + ":" + password + "@" + host + remoteFilePath;
+	}
+}
