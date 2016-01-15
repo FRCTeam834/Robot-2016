ebbd3d77fa09dbbee6b33436c6aa42c1ddcc0401
Ben Zalatan <bzalatan1@gmail.com>
2016-01-15 09:12:56 -0500

    Commented.

--- src/commands/LightsCommand.java
+++ src/commands/LightsCommand.java
@@ -1,3 +1,7 @@
+// Authors:
+// Last Edited: 1/15/2016
+// Description: Command for toggling the lights in the robot.
+
 package commands;
 
 import org.usfirst.frc.team834.robot.*;
@@ -5,30 +9,31 @@ import base.Command;
 import edu.wpi.first.wpilibj.Relay;
 
 public class LightsCommand implements Command {
+	//Variable to represent the robot.
 	private VisualRobot robot;
-	private Relay lights;
+	//Boolean to determine whether or not the lights should be on.
 	private boolean on;
 	
 	public void edit() {
+		//Set values of labels and default values of textboxes in the command edit menu.
 		String[] labels = {"Turn 'on' or 'off'"};
 		String[] values = {on ? "on" : "off"};
 		EditDialog d = new EditDialog(labels,values);		
 		
-		if(values[0].equals("on")) {
+		//If the value of the first textbox is "on", set the "on" boolean to true. If it is "off", turn the "on" boolean to false.
+		if(values[0].equals("on"))
 			on = true;
-		}
-		else if(values[0].equals("off")) {
+		else if(values[0].equals("off"))
 			on = false;
-			
-		}
 	}
 
 	public void execute() throws NullPointerException {
+		//Set the lights to the "on" boolean upon executing.
 		robot.setLights(on);
 	}
 
 	public void setRobot(VisualRobot r) {
+		//Set the robot variable.
 		robot = r;
 	}
-
 }
