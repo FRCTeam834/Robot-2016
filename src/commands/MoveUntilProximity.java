6336c85b4181d07f079afcb0160c1fcd3a4080bf
Ben Zalatan <bzalatan1@gmail.com>
2016-01-15 09:31:53 -0500

    Commented.

--- src/commands/MoveUntilProximity.java
+++ src/commands/MoveUntilProximity.java
@@ -1,3 +1,7 @@
+//Authors: 
+//Last Edited: 1/15/2016
+// Description: Command to move until a proximity is reached.
+
 package commands;
 
 import org.usfirst.frc.team834.robot.VisualRobot;
@@ -7,51 +11,65 @@ import edu.wpi.first.wpilibj.AnalogInput;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class MoveUntilProximity implements Command {
+	//Variable to represent the robot.
 	private VisualRobot robot;
+	//The speed at which the robot should travel, the distane the robot should travel, and the c factor.
 	private double speed, distance, cFactor;
+	//Variable to represent the ultrasonic.
 	private AnalogInput ultrasonic; 
+	//Variable to represent the gyroscope.
 	private AnalogGyro gyro;
 	
 	@Override
 	public void edit() {
+		//Labels and textbox default values to be used in the command edit window.
 		String[] labels = {"Distance", "Speed"};
 		String[] values = {Double.toString(distance), Double.toString(speed)};
 		EditDialog f = new EditDialog(labels, values);
 		
+		//Set distance to first textboxe's value.
 		distance = Double.parseDouble(values[0]);
 		
-		if(Math.abs(Double.parseDouble(values[1])) <= 1.0) {
+		//If the speed is less than or equal to one, set the speed to the second textboxe's value.
+		if(Math.abs(Double.parseDouble(values[1])) <= 1.0)
 			speed = Double.parseDouble(values[1]);
-		}
 	}
 
 	@Override
 	public void execute() throws NullPointerException {
+		//Set the c factor.
 		cFactor = speed / 45.00000001;
 		
+		//Reset the gyro.
 		gyro.reset();
 		
+		//Loop until the desired distance is travelled.
 		while(voltageToDistance(ultrasonic.getVoltage()) < distance && !robot.isDisabled() && robot.isAutonomous()) {
-			double rspeed = speed;
-			double lspeed = speed;
+			//Speed of the left and right wheels.
+			double lspeed = speed, rspeed = speed;
 			
+			//If the gyro's angle is less than zero, change the speed of the right wheel.
 			if(gyro.getAngle() < 0)
 				rspeed -= Math.abs(gyro.getAngle()) / cFactor;
+			//If the gyro's angle is more than zero, change the speed of the left wheel.
 			else if(gyro.getAngle() > 0)
 				lspeed -= Math.abs(gyro.getAngle()) / cFactor;
 			
+			//Set the wheel speeds.
 			robot.setLeftSide(lspeed);
 			robot.setRightSide(rspeed);
 		}
 	}
 	
 	public double voltageToDistance(double voltage) {
+		//Convert voltage returned by the ultrasonic to distance in inches.
 		//http://www.maxbotix.com/documents/LV-MaxSonar-EZ_Datasheet.pdf
 		return ((5 / 512) * distance) * voltage;
 	}
 
 	@Override
 	public void setRobot(VisualRobot r) {
+		//Set the robot, ultrasonic, and gyro variables.
 		robot = r;
 		ultrasonic = (AnalogInput)r.getSensors().get("ultrasonic");
 		gyro = (AnalogGyro) robot.getSensors().get("gyro");
