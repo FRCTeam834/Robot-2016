47c62f74b999291d8915516442ed5f5e62904c61
Ben <11513@172.23.123.28>
2016-02-11 21:01:04 -0500

    stuff

--- src/commands/MoveBackArmCommand.java
+++ src/commands/MoveBackArmCommand.java
@@ -3,38 +3,38 @@ package commands;
 import org.usfirst.frc.team834.robot.Robot;
 import org.usfirst.frc.team834.robot.VisualRobot;
 
+import edu.wpi.first.wpilibj.AnalogGyro;
 import edu.wpi.first.wpilibj.DigitalInput;
 
 public class MoveBackArmCommand {
 	private Robot robot;
-	private DigitalInput topArmInput, bottomArmInput;
+	private AnalogGyro gyro;
 	private boolean direction; //true is up, false is down
+	private double angle;
 	private final double SPEED = 0.3;
 	
 	public void edit() {
-		String[] labels = {"Direction"};
-		String[] values = {Boolean.toString(direction)};
+		String[] labels = {"Direction", "Angle"};
+		String[] values = {Boolean.toString(direction), Double.toString(angle)};
 		EditDialog d = new EditDialog(labels,values);		
 		
 		direction = Boolean.parseBoolean(values[0]);
+		angle = Double.parseDouble(values[1]);
 	}
 
 	public void execute() throws NullPointerException {
-		
-		if(direction) 
-			while(!topArmInput.get())
-				robot.setFeederArm(SPEED);
-		else if(!direction)
-			while(!bottomArmInput.get())
-				robot.setFeederArm(-SPEED);
-		robot.setFeederArm(0);
+		if(direction)
+			while(gyro.getAngle() < angle)
+				robot.setBackArm(SPEED);
+		else
+			while(gyro.getAngle() > angle)
+				robot.setBackArm(-SPEED);
 		//SmartDashboard.putString("DB/String 9", Boolean.toString(isOpen));
 	}
 
 	public void setRobot(VisualRobot r) {
 		robot = (Robot) r;
-		topArmInput = (DigitalInput)robot.getSensors().get("topArmInput");
-		bottomArmInput = (DigitalInput)robot.getSensors().get("bottomArmInput");
+		gyro = (AnalogGyro)robot.getSensors().get("armGyro");
 	}
 	
 	public MoveBackArmCommand() {
