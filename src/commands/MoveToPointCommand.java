dbe198b04cb5f432aee4c2d0e44d4b3cbf390c8d
Ben <11513@172.23.123.28>
2016-02-13 13:50:01 -0500

    Made move to point command.

--- src/commands/MoveToPointCommand.java
+++ src/commands/MoveToPointCommand.java
@@ -4,11 +4,14 @@ import org.usfirst.frc.team834.robot.Robot;
 import org.usfirst.frc.team834.robot.VisualRobot;
 
 import edu.wpi.first.wpilibj.AnalogGyro;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class MoveToPointCommand {
 	private Robot robot;
 	private double displacementX, displacementY, speed;
 	private AnalogGyro gyro;
+	private Encoder LEncoder, REncoder;
 	
 	public void edit() {
 		String[] labels = {"DisplacementX", "DisplacementY", "Speed"};
@@ -23,42 +26,69 @@ public class MoveToPointCommand {
 	public void execute() throws NullPointerException {
 		gyro.reset();
 		
-		if(!direction)
-			while(angle > gyro.getAngle())
-			{
-				robot.setLeftSide(speed * (radius / (radius + WIDTH)));
-				robot.setRightSide(speed);
-			}
-		else
-			while(angle < gyro.getAngle())
-			{
+		double angle = getAngle(), distance = getDistance();
+		
+		if(angle > 0) {
+			while (gyro.getAngle() < angle && !robot.isDisabled() && robot.isAutonomous()) {
+				robot.setRightSide(-speed);
 				robot.setLeftSide(speed);
-				robot.setRightSide(speed * (radius / (radius + WIDTH)));
+				SmartDashboard.putString("DB/String 2", Double.toString(gyro.getAngle()));
+
 			}
+			robot.stop();
+		}
+		else if(angle < 0) {
+			while (gyro.getAngle() > angle && !robot.isDisabled() && robot.isAutonomous()) {
+				robot.setRightSide(speed);
+				robot.setLeftSide(-speed);
+				SmartDashboard.putString("DB/String 2", Double.toString(gyro.getAngle()));
+
+			}
+			robot.stop();
+		}
+		
+		REncoder.reset();
+		LEncoder.reset();
+		while(LEncoder.getDistance() < distance && REncoder.getDistance() < distance && !robot.isDisabled() && robot.isAutonomous())
+		{
+			robot.setLeftSide(speed);
+			robot.setRightSide(speed);
+		}
+		robot.setLeftSide(0);
+		robot.setRightSide(0);
 	}
 
 
 	public void setRobot(VisualRobot r) {
 		robot = (Robot)r;
 		gyro = (AnalogGyro)robot.getSensors().get("gyro");
+		LEncoder = (Encoder) robot.getSensors().get("leftEncoder");
+		REncoder = (Encoder) robot.getSensors().get("rightEncoder");
 	}
 	
-	private double getDistance()
-	{
+	private double getDistance() {
 		return Math.sqrt(displacementX * displacementX + displacementY * displacementY);
 	}
-	private double getAngle()
-	{
-		return Math.atan2(displacementY, displacementX) * 180 / Math.PI;
+	private double getAngle() {
+		return 90 - Math.atan2(displacementY, displacementX) * 180 / Math.PI;
+		
 	}
 	
 	public MoveToPointCommand() {
 	}
 
+	/**
+	 * 
+	 * @param dx The displacement on the x axis.
+	 * @param dy The displacement on the y axis.
+	 * @param s The speed at which to move.
+	 * @param r The robot.
+	 */
 	public MoveToPointCommand(double dx, double dy, double s, VisualRobot r) {
 		displacementX = dx;
-		displacementY = dx;
+		displacementY = dy;
 		speed = s;
+		if(r!= null)
 		setRobot(r);
 	}
 }
